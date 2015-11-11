f81a1c629078ccf4d283410666b8bdd4999971a8
Thiago Barros <thiagosbarros02@gmail.com>
2015-11-11 20:16:26 -0200

    Create connection class and include mysql connection jar into project

--- src/connection/BDConnection.java
+++ src/connection/BDConnection.java
@@ -1,5 +1,55 @@
 package connection;
 
-public class BDConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class BDConnection {
+	 
+	private static BDConnection uniqueInstance;
+	
+	private final String ADAPTER = "mysql";
+	private final String HOST = "";
+	private final String PORT = "3306";
+	private final String DATABASE = "";
+	private final String USERNAME = "";
+	private final String PASSWORD = "";
+	protected Connection connection;
+	
+	//private contructor for Singleton pattern
+	private BDConnection() throws SQLException {
+		this.connection = null;
+		open();
+	}
+	
+	//public method for Singleton pattern
+	public static synchronized BDConnection getInstance(){
+		if (uniqueInstance == null)
+			try {
+				uniqueInstance = new BDConnection();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return uniqueInstance;
+	}
+
+	//Open connection with database
+	public void open() throws SQLException {
+		if (this.connection == null) {
+			String connection = 
+							"jdbc:" + this.ADAPTER + 	
+							"://" 	+ this.HOST + 
+							":" 	+ this.PORT + 
+							"/" 	+ this.DATABASE;
+			this.connection = DriverManager.getConnection(connection,this.USERNAME,this.PASSWORD);
+		}
+	}
+
+	//Close connection with database
+	public void close() throws SQLException {
+		if (this.connection != null) {
+			this.connection.close();
+		}
+	}	
 }
